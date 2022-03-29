0c967831dcb8eb0af7e756226884b6779dcf95a6
Lerqiu <mcwk810@gmail.com>
2022-03-29 05:52:21 +0200

    Stage 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import Bar from "./Components/MainBar/Bar";
 import "./App.css";
 import BudgetContextProvider from "./Context/BudgetContext";
-import BuilderContextProvider from "./Context/BuilderContext";
-import List from "./Components/List/List";
-import Status from "./Components/Status/Status";
 
 import Stack from "@mui/material/Stack";
 import Container from "@mui/material/Container";
@@ -18,14 +14,6 @@ import Profit from "./Routing/Profit";
 const App = () => {
   return (
     <BudgetContextProvider>
-      {/*
-          <BuilderContextProvider>
-            <Bar />
-          </BuilderContextProvider>
-          <Status />
-          <List />
-        </Stack>
-      </Container> */}
       <BrowserRouter>
         <Header />
         <Container maxWidth="lg">
--- src/Components/MainBar/Bar.tsx
+++ src/Components/MainBar/Bar.tsx
@@ -1,44 +1,83 @@
 import React, { useContext } from "react";
-import { BuilderContext } from "../../Context/BuilderContext";
-import InputDescription from "./Inputs/InputDescription";
-import InputNumber from "./Inputs/InputNumber";
-import Select from "./SelectType/Select";
-
-import "./Bar.css";
+import './Bar.css';
 
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { EntryType } from "../Entry/RawEntry";
+import { useForm, Controller } from "react-hook-form";
+import TextField from "@material-ui/core/TextField";
+import { BudgetContext } from "../../Context/BudgetContext";
+
+type FormValues = {
+  Description: string;
+  Value: string;
+};
 
 const Bar = (props: { type: EntryType }) => {
-  const { commit, addType } = useContext(BuilderContext);
-  addType(props.type);
+  const { handleSubmit, control, reset } = useForm<FormValues>();
+  const { add } = useContext(BudgetContext);
+
+  const onSubmit = (data: FormValues) => {
+    add({
+      description: data.Description,
+      value: data.Value,
+      type: props.type,
+    });
+    reset();
+  };
 
   return (
     <Box sx={{ flexGrow: 1, borderRadius: 16 }} className="BlueBorder">
-      <Grid
-        container
-        spacing={2}
-        justifyContent="center"
-        alignItems="center"
-        sx={{ mt: 2, mb: 2 }}
-      >
-        <Grid item xs={6}>
-          <InputDescription />
+      <form>
+        <Grid
+          container
+          spacing={2}
+          justifyContent="center"
+          alignItems="center"
+          sx={{ mt: 2, mb: 2 }}
+        >
+          <Grid item xs={6}>
+            <Controller
+              name="Description"
+              control={control}
+              defaultValue=""
+              rules={{required: true, minLength:3, maxLength:20}}
+              render={({ field }) => (
+                <TextField
+                  label="Description"
+                  variant="outlined"
+                  fullWidth={true}
+                  {...field}
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs={1}>
+            <Controller
+              name="Value"
+              control={control}
+              defaultValue="0"
+              rules={{required: true, pattern :/^[0-9]+[.]{0,1}[0-9]{0,2}$/i}}
+              render={({ field }) => (
+                <TextField
+                  inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                  {...field}
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <Button
+              onClick={handleSubmit(onSubmit)}
+              variant="contained"
+              size="medium"
+            >
+              Add record
+            </Button>
+          </Grid>
         </Grid>
-        {/* <Grid item xs={2}>
-          <Select />
-        </Grid> */}
-        <Grid item xs={1}>
-          <InputNumber />
-        </Grid>
-        <Grid item xs={2}>
-          <Button onClick={commit} variant="contained" size="medium">
-            Add record
-          </Button>
-        </Grid>
-      </Grid>
+      </form>
     </Box>
   );
 };
